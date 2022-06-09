bc4a77ea5eaa215d179d76fcd845fd4e219f2fcb
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-06-09 10:40:18 +0300

    enemy spawn amount increases as score increases

--- Pelit/TokaViikko/Assets/Code/ScoreManager.cs
+++ Pelit/TokaViikko/Assets/Code/ScoreManager.cs
@@ -7,9 +7,46 @@ public class ScoreManager : MonoBehaviour
 {
     private int score;
 
+    [SerializeField]
+    private GameObject[] spawners;
+
+    private enum SpeedState
+    { normal, fast, faster, fastest };
+
+    private SpeedState state;
+
     public void AddScore(int amount)
     {
         score += amount;
+        if(score >= 50 && state == SpeedState.normal)
+        {
+            state = SpeedState.fast;
+            foreach(GameObject spawner in spawners)
+            {
+                spawner.GetComponent<Spawner>().SpeedUp(1);
+            }
+        }
+        if(score >= 100 && state == SpeedState.fast)
+        {
+            state = SpeedState.faster;
+            foreach(GameObject spawner in spawners)
+            {
+                spawner.GetComponent<Spawner>().SpeedUp(1);
+            }
+        }
+        if(score >= 150 && state == SpeedState.faster)
+        {
+            state = SpeedState.fastest;
+            foreach(GameObject spawner in spawners)
+            {
+                spawner.GetComponent<Spawner>().SpeedUp(1);
+            }
+        }
+    }
+
+    void Start()
+    {
+        state = SpeedState.normal;
     }
 
     private void Update()
