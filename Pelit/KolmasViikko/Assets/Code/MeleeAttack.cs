7a6b04150a2d2516d0eb4d59102e4e73b2be3a02
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-06-21 21:26:08 +0300

    attacking (no animation yet)

---
+++ Pelit/KolmasViikko/Assets/Code/MeleeAttack.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D hitbox;
+    private bool attacking = false;
+
+    [SerializeField]
+    private float attackDuration = 0.3f;
+    private float attackDurationtemp;
+
+    void Start()
+    {
+        attackDurationtemp = attackDuration;
+    }
+
+    void Update()
+    {
+        if(attacking)
+        {
+            attackDurationtemp -= Time.deltaTime;
+            if(attackDurationtemp <= 0)
+            {
+                attacking = false;
+                hitbox.enabled = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+                attackDurationtemp = attackDuration;
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("swing");
+            attacking = true;
+            hitbox.enabled = true;
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
