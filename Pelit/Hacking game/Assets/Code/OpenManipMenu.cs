ab21a024e0319a787e8a29712a19db2258a35c39
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-06-29 19:19:33 +0300

    we do a little clicking

---
+++ Pelit/Hacking game/Assets/Code/OpenManipMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenManipMenu : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject menu;
+    private GameObject spawnedMenu;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(spawnedMenu == null)
+        {
+            spawnedMenu = Instantiate(menu, new Vector3(transform.position.x, transform.position.y + 2, 0), transform.rotation);
+        }
+    }
+}
