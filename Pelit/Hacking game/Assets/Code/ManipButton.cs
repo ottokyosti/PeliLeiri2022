71ffa832ee81b7cb42ab7e1b57a30849fa8e4104
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-06-30 14:11:42 +0300

    force push thingy

--- Pelit/Hacking game/Assets/Code/ManipButton.cs
+++ Pelit/Hacking game/Assets/Code/ManipButton.cs
@@ -7,10 +7,16 @@ public class ManipButton : MonoBehaviour, IPointerDownHandler
 {
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("click");
+        Debug.Log(gameObject.tag);
         if(gameObject.tag == "gravityButton")
         {
+            Debug.Log("click1");
             GetComponentInParent<ManipObject>().FlipGravity();
+        }
+        else if(gameObject.tag == "forceButton")
+        {
+            Debug.Log("click2");
+            GetComponentInParent<ManipObject>().ForceAdd();
         }
     }
 }
--- Pelit/Hacking game/Assets/Code/ManipObject.cs
+++ Pelit/Hacking game/Assets/Code/ManipObject.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class ManipObject : MonoBehaviour
 {
+    private GameObject player;
+    [SerializeField]
+    private float force = 50;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     public void FlipGravity()
     {
         GetComponent<Rigidbody2D>().gravityScale *= -1;
+    }
+
+    public void ForceAdd()
+    {
+        if(player.transform.position.x <= gameObject.transform.position.x)
+        {
+            Debug.Log("forward");
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(1,0,0) * force);
+        }
+        else
+        {
+            Debug.Log("back");
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(-1,0,0) * force);   
+        }
     }
 }
