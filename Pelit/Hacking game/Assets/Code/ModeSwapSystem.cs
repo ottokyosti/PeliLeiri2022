5ed692390d5788a082055994deed6f560eb29b74
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-07-05 13:31:57 +0300

    tab to switch, number key to choose code

--- Pelit/Hacking game/Assets/Code/ModeSwapSystem.cs
+++ Pelit/Hacking game/Assets/Code/ModeSwapSystem.cs
@@ -6,21 +6,70 @@ public class ModeSwapSystem : MonoBehaviour
 {
     private bool inManip = false;
 
+    private enum AppliedManip { none, first, second, third, fourth };
+
+    private AppliedManip CurrentManip = AppliedManip.none;
+
     [SerializeField]
     private GameObject ManipUI;
 
-    public void SwapMode()
+    void Update()
     {
-        if(!inManip)
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwapMode();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) && inManip)
+        {
+            ManipMode(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && inManip)
+        {
+            ManipMode(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && inManip)
+        {
+            ManipMode(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && inManip)
+        {
+            ManipMode(4);
+        }
+    }
+
+    private void SwapMode()
+    {
+        if (!inManip)
         {
             Time.timeScale = 0;
             ManipUI.SetActive(true);
         }
-        else if(inManip)
+        else if (inManip)
         {
             Time.timeScale = 1;
             ManipUI.SetActive(false);
         }
         inManip = !inManip;
+        Debug.Log(inManip);
+    }
+
+    private void ManipMode(int input)
+    {
+        switch (input)
+        {
+            case 1:
+                CurrentManip = AppliedManip.first;
+                break;
+            case 2:
+                CurrentManip = AppliedManip.second;
+                break;
+            case 3:
+                CurrentManip = AppliedManip.third;
+                break;
+            case 4:
+                CurrentManip = AppliedManip.fourth;
+                break;
+        }
+        Debug.Log(CurrentManip);
     }
 }
