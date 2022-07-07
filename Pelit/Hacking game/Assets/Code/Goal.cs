06101b71c4f04da208d493c99515942a489def03
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-07-07 18:27:18 +0300

    test level 2

---
+++ Pelit/Hacking game/Assets/Code/Goal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //temp
+            SceneManager.LoadScene(3);
+        }
+    }
+}
