abe62e77649ef7d08db6e98ce0db8304d020a4a5
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2022-07-11 15:06:20 +0300

    Pause menu implemented (can add buttons if need be)

---
+++ Pelit/Hacking game/Assets/Code/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool spamInhibitor = true;
+
+    [SerializeField] private GameObject canvas;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.activeSelf)
+            {
+                Resume();
+            }
+            else if (!(canvas.activeSelf) && spamInhibitor)
+            {
+                Time.timeScale = 0;
+                canvas.SetActive(true);
+            }
+        }
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        spamInhibitor = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        canvas.SetActive(false);
+        spamInhibitor = false;
+        StartCoroutine(WaitTime());
+    }
+
+    public void Mainmenu()
+    {
+        //Scenemanager.LoadScene(0);
+    }
+}
