d4a02c56860facc18a800d08d915f65e933ecb16
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2022-07-12 21:22:46 +0300

    Shooting enemy implemented, can shoot and kill player

---
+++ Pelit/Hacking game/Assets/Code/Projectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Vector3 direction;
+    public Vector3 _direction
+    {
+        get { return direction; }
+        set { direction = value; }
+    }
+
+    [SerializeField] private float speed;
+
+    private float destroyTimer = 10f;
+
+    private void Update()
+    {
+        if (destroyTimer <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+        else 
+        {
+            destroyTimer -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
