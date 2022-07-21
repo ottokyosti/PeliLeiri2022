58856a83c2a40c000a13cd1729b8778a1cee420d
Joonas Järvinen <joonas.jarvinen@tuni.fi>
2022-07-21 14:05:07 +0300

    enemy die particle

--- Pelit/Hacking game/Assets/Code/CrushEnemy.cs
+++ Pelit/Hacking game/Assets/Code/CrushEnemy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CrushEnemy : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject deathParticle;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Enemy")
         {
+            Instantiate(deathParticle, col.gameObject.transform.position, col.gameObject.transform.rotation);
             Destroy(col.gameObject);
         }
     }
